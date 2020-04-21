7b280c5fcdae101ee265ccf8a19f86f4ee26cdfe
legomaster5868 <yoel112000@gmail.com>
2020-04-20 20:02:15 -0600

    añadiendo altas

--- proj_Nomina/src/Formas/FrmMantDeptos.java
+++ proj_Nomina/src/Formas/FrmMantDeptos.java
@@ -90,6 +90,11 @@ public class FrmMantDeptos extends javax.swing.JInternalFrame {
         btnAlta.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
         btnAlta.setText("Alta");
         btnAlta.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btnAlta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAltaActionPerformed(evt);
+            }
+        });
 
         btnBaja.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
         btnBaja.setText("Baja");
@@ -315,8 +320,24 @@ public class FrmMantDeptos extends javax.swing.JInternalFrame {
             txtEstadoDepto.setText("");
             
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.toString());
         }    
     }//GEN-LAST:event_btnBajaActionPerformed
+
+    private void btnAltaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAltaActionPerformed
+        try{
+            java.sql.Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/planilla_sys", "root", "");
+            java.sql.PreparedStatement pst = cn.prepareStatement("insert into departamento values(?,?,?)");
+            pst.setString(1, txtCodDepto.getText().trim());
+            pst.setString(2, txtNomDepto.getText().trim());
+            pst.setString(3, txtEstadoDepto.getText().trim());
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Registro Realizado!");
+        }
+        catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex.toString());
+        }
+    }//GEN-LAST:event_btnAltaActionPerformed
     String iTexto="";
     private void ObtenerInicial(String texto){
         for(int i=0; i<5; i++){
