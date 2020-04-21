5846c5b5b7cee21e9acc07f49789bde151032060
EngineerDV <engdv2017@gmail.com>
2020-04-20 18:32:41 -0600

    Añadiendo Contraseña

--- proj_Nomina/src/Formas/FrmMantDeptos.java
+++ proj_Nomina/src/Formas/FrmMantDeptos.java
@@ -252,7 +252,7 @@ public class FrmMantDeptos extends javax.swing.JInternalFrame {
     private void btnCambioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCambioActionPerformed
         try{
             String ID = txtCodDepto.getText().trim();
-            Connection cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/planilla_sys", "root", "");
+            Connection cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/planilla_sys", "root", "informaticdv2016");
             PreparedStatement pst = (PreparedStatement) cn.prepareStatement("UPDATE departamento SET nombre_departamento=?, estado_departamento=? WHERE id_departamento='"+ID+"'");
             pst.setString(1, txtNomDepto.getText().trim());
             pst.setString(2, txtEstadoDepto.getText().trim());
@@ -265,7 +265,7 @@ public class FrmMantDeptos extends javax.swing.JInternalFrame {
 
     private void btnBuscarxCodActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarxCodActionPerformed
         try{
-            Connection cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/planilla_sys", "root", "");
+            Connection cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/planilla_sys", "root", "informaticdv2016");
             PreparedStatement pst = (PreparedStatement) cn.prepareStatement("select * from departamento where id_departamento = ?");
             pst.setString(1, txtBuscaxCod.getText().trim());
             ResultSet rs = pst.executeQuery();
@@ -284,7 +284,7 @@ public class FrmMantDeptos extends javax.swing.JInternalFrame {
 
     private void btnBuscarxNomActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarxNomActionPerformed
         try{
-            Connection cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/planilla_sys", "root", "");
+            Connection cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/planilla_sys", "root", "informaticdv2016");
             PreparedStatement pst = (PreparedStatement) cn.prepareStatement("select * from departamento where nombre_departamento = ?");
             pst.setString(1, txtBuscaxNom.getText().trim());
             ResultSet rs = pst.executeQuery();
@@ -304,7 +304,7 @@ public class FrmMantDeptos extends javax.swing.JInternalFrame {
     private void btnBajaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBajaActionPerformed
         // TODO add your handling code here:
         try {
-             java.sql.Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/planilla_sys", "root", "");
+             java.sql.Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/planilla_sys", "root", "informaticdv2016");
             java.sql.PreparedStatement pst = cn.prepareStatement("delete from departamento where id_departamento = ?");
             
             pst.setString(1, txtBuscaxCod.getText().trim());
